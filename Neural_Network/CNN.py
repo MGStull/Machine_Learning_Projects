4b4d268f61435b7b5bb2588d9e8342e780ab7e19
mstull <mstull@westmont.edu>
2025-06-02 14:54:08 -0700

    Implementing Epochs

--- Neural_Network/CNN.py
+++ Neural_Network/CNN.py
@@ -3,15 +3,18 @@ from keras.datasets import fashion_mnist
 from tensorflow.keras.models import Sequential
 import matplotlib.pyplot as plt
 import numpy as np
-
+import random as rand
 
 
 class DenseLayer:
     def __init__(self, input_size, output_size):
         self.W = np.random.randn(input_size, output_size) * 0.01
         self.b = np.zeros((1, output_size))
+        self.dW = None
+        self.dB = None
 
     def forward(self, x):
+        self.x = np.copy(x)
         self.z = x@self.W + self.b
         self.a = self._apply_activation(self.z)
         return self.a
@@ -20,6 +23,11 @@ class DenseLayer:
         return np.exp(-x)
     def activationDerivative(self,x):
         return -np.exp(-x)
+    def update(self,stepSize=.01):
+        self.W -= self.dW*stepSize
+        self.b -= self.db*stepSize
+
+        
 
         
 class Neural_Network:
@@ -38,49 +46,87 @@ class Neural_Network:
         return x
     
     def backprop(self,batch):
-        predicted = self.forward(batch.input)
+        batch.predicted = self.forward(batch.input)
         expected = batch.expected
-        self.weightGradient(expected,predicted)
+        self.weightGradient(batch,self.layers)
+        for layer in self.layers:
+            layer.update()
         return self.error(batch)
 
     def error(self,batch):
-        return np.linalg.norm(batch.predicted-batch.expeceted,ord=2)/batch.size
+        return np.linalg.norm(batch.predicted-batch.expected,ord=2)/batch.size
     def errorGradient(self,batch):
         return 2*(batch.predicted-batch.expected)/batch.size
 
     #This is done by the matrix linear algebra definiont of backword pass formula for Neural Networks./ A little bit overkill but we will see how my computer does
-    def weightGradient(self,expected,predicted,weights):
-        pass
-
-    def errorGradient(expected,predicted,weights):
-        pass
-    
-    def biasGradient(self,expected,predicted,weights):
-        pass
-
+    def weightGradient(self,batch,weights):
+        delta = self.errorGradient(batch)
+        for layer in reversed(self.layers):
+            dz=delta*layer.activationDerivative(layer.z)
+            dW = layer.x.T @ dz
+            db = np.sum(dz,axis=0,keepdims=True)
+            delta = dz@layer.W.T
+            layer.dW =dW
+            layer.db =db
 
 (x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
-
 class batch:
-    def __init__(self,size):
-        self.size=size
-        self.input = self.Grab(size)[0]
-        self.expected = self.Grab(size)[1]
-    #Pull Data in a K cross format to prevent overfitting
-    def Grab(self,size):
-        # Grab the first `size` samples from test data
-        X_batch = x_test[:size]  
-        Y_batch = y_test[:size]  
-        X_batch = X_batch.reshape(size, -1)/255  # Normalize pixel values to [0, 1]
-        Y_batch_one_hot = np.zeros((size, 10))
-        Y_batch_one_hot[np.arange(size), Y_batch] = 1
-        return [X_batch, Y_batch_one_hot]
+        def __init__(self,indeces):
+            self.size=len(indeces)
+            self.input,self.expected = self.Grab(indeces)
+        #Pull Data in a K cross format to prevent overfitting
+        def Grab(self,indeces):
+            # Grab the first `self.size` samples from test data
+            X_batch = np.zeros(self.size)
+            Y_batch = np.zeros(self.size)
+            for i in range(0,len(indeces)):    
+                X_batch[i] = x_train[indeces[i]]  
+                Y_batch[i] = y_train[indeces[i]]  
+            X_batch = X_batch.reshape(self.size, -1)/255  # Normalize pixel values to [0, 1]
+            Y_batch_one_hot = np.zeros((self.size, 10))
+            Y_batch_one_hot[np.arange(self.size), Y_batch] = 1
+            return [X_batch, Y_batch_one_hot]
+class Epoch:
+    def __init__(self,k):
+        self.indicesList = [[]]*k
+        self.batches = []
+        randHold = list(range(0,60000))
+        print(self.indicesList)
+        print(int(len(x_train)/k))
+        for i in range(0,k):
+            for j in range(0,int(len(x_train)/k)):
+                #print("randHold Length: ",len(randHold))
+                x =rand.randint(0,len(randHold)-1)
+                #print("Random Index: ",x)
+                #print("self.indicesList[i]",print(len(self.indicesList),"Does not match: ",i))
+                self.indicesList[i].append(randHold.pop(x))
+        print("Complete",len(randHold),len(self.indicesList),self.indicesList[1])
+        for i in self.indicesList:
+            self.batches.append(batch(i))
+    def randTrain(self,NN):
+        for i in self.batches:
+            NN.backprop(i) 
+        print("Complete")  
 
 
 NN = Neural_Network([
-    [784,784],
-    [784,1]
+    [784,128],
+    [128,128],
+    [128,64],
+    [64,32],
+    [32,10]
     ])
-print(NN.backprop(batch(3)))
 
 
+def TEST(testSize,NN):
+    # Grab the first `size` samples from test data
+    X_batch = x_test[:testSize]
+    X_batch = X_batch.reshape(testSize, -1)/255 
+    Y_batch =y_test[:testSize]
+    for i in range(testSize):
+        print(NN.guess(X_batch[i]))
+        if NN.guess(X_batch[i]) == Y_batch[i]:
+            sum +=1
+    return (sum/testSize)
+Epoch(100).randTrain(NN)
+TEST(NN,100)
